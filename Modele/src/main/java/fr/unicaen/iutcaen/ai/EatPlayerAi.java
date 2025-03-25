6f309d3890cebb2382f898b739332dcc8e12666f
courtoi223 <courtoi223@unicaen.fr>
2025-03-25 17:48:19 +0100

    first

---
+++ Modele/src/main/java/fr/unicaen/iutcaen/ai/EatPlayerAi.java
@@ -0,0 +1,12 @@
+package fr.unicaen.iutcaen.ai;
+
+import fr.unicaen.iutcaen.model.Player;
+import fr.unicaen.iutcaen.model.World;
+import javafx.geometry.Point2D;
+
+public class EatPlayerAi implements AIBehavior{
+    @Override
+    public Point2D computeDirection(Player player, World world) {
+        return null;
+    }
+}
