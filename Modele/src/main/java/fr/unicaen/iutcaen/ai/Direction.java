d1e44a26af939775e5165cdd16c73370d5cfe63d
Idea1000 <brault231@etu.unicaen.fr>
2025-03-26 17:01:54 +0100

    adding ai

---
+++ Modele/src/main/java/fr/unicaen/iutcaen/ai/Direction.java
@@ -0,0 +1,5 @@
+package fr.unicaen.iutcaen.ai;
+
+public enum Direction {
+    NORTH, SOUTH, EAST, WEST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST;
+}
---
+++ Modele/src/main/java/fr/unicaen/iutcaen/ai/EatPlayerAi.java
@@ -0,0 +1,41 @@
+package fr.unicaen.iutcaen.ai;
+
+import fr.unicaen.iutcaen.model.Point;
+
+/**
+ * Define the AI behavior as eat the player
+ * @author courtoi223, Idea1000
+ */
+public class EatPlayerAi implements AIBehavior {
+
+    /**
+     * the X position of the target
+     */
+    private double targetX = 0;
+
+    /**
+     * the Y position of the target
+     */
+    private double targetY = 0;
+
+    /**
+     * Move the IA to the desired target
+     * @param ia
+     */
+    @Override
+    public void move(IA ia) {
+        Point direction = new Point(targetX, targetY);
+        ia.getCells().move(direction);
+    }
+
+    /**
+     * Update the IA target
+     * @param ia
+     */
+    @Override
+    public void update(IA ia) {
+        Point pos = ia.getTarget().getPosition();
+        targetX = pos.getX();
+        targetY = pos.getY();
+    }
+}
